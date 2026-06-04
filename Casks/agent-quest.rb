b7e72a5bc4514d777f19605a78ec8bddadd3f65c
zodius <zodius.chen@gmail.com>
2026-06-04 16:30:19 +0800

    feat: agent-quest v0.2.0

--- Casks/agent-quest.rb
+++ Casks/agent-quest.rb
@@ -1,6 +1,6 @@
 cask "agent-quest" do
-  version "0.1.0"
-  sha256 "ebcb8d34d8e6b6692679e6487891b1afcf1e48a06d6400413e5e5a78c4022b47"
+  version "0.2.0"
+  sha256 "25a12436b90f21affa3fd8800784df9ed07ceed36a19278f88ec90ad708c3dd4"
 
   url "https://github.com/zodius-chtsec/agent-quest/releases/download/v#{version}/agent-quest_#{version}_universal.dmg"
   name "agent-quest"
